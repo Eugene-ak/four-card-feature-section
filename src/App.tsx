fd597ae386e3424ccea1f6385f560d6c8d34dff4
Eugene-ak <amedioreugene@gmail.com>
2024-04-19 16:59:16 +0000

    Project initialisation and setup

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import "./styles.scss";
+
+function App() {
+  return (
+    <>
+      <body>
+        <h1>Reliable, efficient delivery</h1>
+        <h1>Powered by Technology</h1>
+
+        <p>
+          Our Artificial Intelligence powered tools use millions of project data
+          points to ensure that your project is successful
+        </p>
+
+        <h2>Supervisor</h2>
+        <p>Monitors activity to identify project roadblocks</p>
+
+        <h2>Team Builder</h2>
+        <p>
+          Scans our talent network to create the optimal team for your project
+        </p>
+
+        <h2>Karma</h2>
+        <p>Regularly evaluates our talent to ensure quality</p>
+
+        <h2>Calculator</h2>
+        <p>Uses data from past projects to provide better delivery estimates</p>
+
+        <footer>
+          <p className="attribution">
+            Challenge by{" "}
+            <a
+              href="https://www.frontendmentor.io?ref=challenge"
+              target="_blank"
+            >
+              Frontend Mentor
+            </a>
+            . Coded by <a href="#">Your Name Here</a>.
+          </p>
+        </footer>
+      </body>
+    </>
+  );
+}
+
+export default App;
